19213fc082ddda2a626c179e0b33831b4ffd72bf
박정현 <ros28081@naver.com>
2022-10-10 15:20:22 +0900

    [BOJ] 2869 달팽이는 올라가고 싶다
    https://www.acmicpc.net/problem/2869

---
+++ src/main/java/week3/BOJ_2869/jhpark_2869.java
@@ -0,0 +1,29 @@
+package BOJ_2869;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class jhpark_2869 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        int days = 0;
+
+        int day = (V - B) / (A - B);
+
+        // 나머지가 있을 경우 (잔여 길이가 있을 경우)
+        if ((V - B) % (A - B) != 0) {
+            day++;
+        }
+        System.out.println(day);
+
+    }
+}
