9d67e7323d9ba6d25ecf3a1fe4f69e962da85d53
박정현 <ros28081@naver.com>
2022-12-24 01:02:22 +0900

    [BOJ] 10430 나머지
    단계별 풀이 완주용 https://www.acmicpc.net/problem/10430

---
+++ src/main/java/week8/BOJ_$10430/jhpark_10430.java
@@ -0,0 +1,27 @@
+package main.java.week8.BOJ_$10430;
+
+// 단순 사칙연산
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class jhpark_10430 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B%C))%C);
+
+    }
+}
