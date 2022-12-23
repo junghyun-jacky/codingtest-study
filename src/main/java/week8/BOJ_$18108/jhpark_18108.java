1af78523946a9d34fec5345cae3b65576bba7eb9
박정현 <ros28081@naver.com>
2022-12-24 01:03:10 +0900

    [BOJ] 18108 1998년생인 내가 태국에서는 2541년생?!
    단계별 풀이 완주용 https://www.acmicpc.net/problem/18108

---
+++ src/main/java/week8/BOJ_$18108/jhpark_18108.java
@@ -0,0 +1,19 @@
+package main.java.week8.BOJ_$18108;
+
+// 단순 사칙연산
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class jhpark_18108 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int y = Integer.parseInt(br.readLine());
+
+        System.out.println(y - 543);
+    }
+}
