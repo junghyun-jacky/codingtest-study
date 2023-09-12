ee3246e8ca902863617e5b4368b9b5bc739b6cc3
junghyun Park <77378847+junghyun-jacky@users.noreply.github.com>
2023-09-12 10:52:35 +0900

    [level 2] Title: 더 맵게, Time: 1510.43 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        
+        for(int i=0; i<scoville.length; i++) {
+            heap.add(scoville[i]);
+        }
+        
+        int count = 0;
+        while(heap.size() > 1 && heap.peek() < K) {
+            if(heap.size() < 1 && heap.peek() < K) {
+                count = -1;
+                break;
+            }
+            int minScoville = heap.poll() + (heap.poll() * 2);
+            heap.add(minScoville);
+            count++;
+        }
+        if(heap.size() == 1 && heap.peek() < K) {
+            count = -1;
+        }
+        
+        int answer = count;
+        return answer;
+    }
+}
